70788faf7562bdee30da368721db20113c5c27e3
keerthivasan123 <keerthivasan287@gmail.com>
2018-12-06 20:23:39 +0530

    Added 4 models

--- project/admin.py
+++ project/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Project,ProjectPhase,ProjectAllocationPlan,ProjectAssignmentPlan
 
-# Register your models here.
+admin.site.register(Project)
+admin.site.register(ProjectPhase)
+admin.site.register(ProjectAssignmentPlan)
+admin.site.register(ProjectAllocationPlan)
