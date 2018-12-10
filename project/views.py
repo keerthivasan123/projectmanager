6477e0718cd0e7e87829966468868f821e7f8fb5
keerthivasan123 <keerthivasan287@gmail.com>
2018-12-10 18:05:16 +0530

    Added 4 models

--- project/views.py
+++ project/views.py
@@ -1,3 +1,35 @@
+from django.shortcuts import render,get_list_or_404
+from django.http import HttpResponse
+from .models import Project,ProjectAllocationPlan,ProjectPhase,ProjectAssignmentPlan
 from django.shortcuts import render
 
-# Create your views here.
+def projectListPage(request):
+    all_project=Project.objects.all()
+    context = {'all_project'all_project}
+    return render(request,'project/index.html',context)
+
+def projectDetailPage(request,project_id):
+    project=get_list_or_404(Project,pk=project_id)
+    return render(request,'project/details.html',{'project':project})
+
+def phaseDefinitionPage(request):
+    all_project_phase=Album.objects.all()
+    context = {'all_albums':all_albums}
+    return render(request,'music/index.html',context)
+
+def resourseAssignmentPage(request,album_id):
+    album=get_list_or_404(Album,pk=album_id)
+    return render(request,'music/details.html',{'album':album})
+
+def resourseAllocationPage(request):
+    all_albums=Album.objects.all()
+    context = {'all_albums':all_albums}
+    return render(request,'music/index.html',context)
+
+def resourseLoadReport(request,album_id):
+    album=get_list_or_404(Album,pk=album_id)
+    return render(request,'music/details.html',{'album':album})
+def resoursePlanReport(request):
+    all_albums=Album.objects.all()
+    context = {'all_albums':all_albums}
+    return render(request,'music/index.html',context)
